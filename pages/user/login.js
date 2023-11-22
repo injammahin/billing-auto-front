609754a8a7cf0fd2cabb3f6df936304ff9459810
injammahin <injammahin5507@gmail.com>
2023-11-22 23:30:42 +0600

    up

--- pages/user/login.js
+++ pages/user/login.js
@@ -1,87 +1,75 @@
-// import { axios } from "axios";
-// import React, { useState } from "react";
-
-// const login = () => {
-//   const [email, setEmail] = useState("");
-//   const [password, setpassword] = useState("");
-//   const handleSubmit = async () => {
-//     let x = await axios.post("http://localhost:2000/auth/signin", {
-//       email,
-
-//       password,
-//     });
-//     console.log(x.data);
-//   };
-//   return (
-//     <div>
-//       <label>
-//         email:
-//         <input
-//           onChange={(e) => setEmail(e.target.value)}
-//           type="email"
-//           name="password"
-//         />
-//       </label>
-//       <br />
-//       <label>
-//         password:
-//         <input
-//           onChange={(e) => setpassword(e.target.value)}
-//           type="password"
-//           name="password"
-//         />
-//       </label>
-//       <br />
-//       <button onClick={handleSubmit}> Signin</button>
-//     </div>
-//   );
-// };
-
-// export default login;
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-const registrations = () => {
+export default function Login() {
   const [email, setEmail] = useState("");
-  const [data, setData] = useState();
-  const [password, setpassword] = useState("");
+  const [password, setPassword] = useState("");
+  const [errormsg, setErrors] = useState({});
+  const [loginError, setLoginError] = useState("");
 
-  const handleSubmit = async () => {
-    let x = await axios.post("http://localhost:2000/auth/signin", {
-      email,
+  const formValidation = () => {
+    const errormsg = {};
+    if (email.trim() === "") {
+      errormsg.email = "Email is required";
+    } else if (!/^\S+@\S+\.\S+$/.test(email)) {
+      errormsg.email = "Invalid email format";
+    }
+    if (password.trim() === "") {
+      errormsg.password = "Password is required";
+    }
+    setErrors(errormsg);
+    return Object.keys(errormsg).length === 0;
+  };
 
-      password,
-    });
-    console.log(x.status);
-    if (x.status == 201) {
-      window.location.href = "userpage";
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    if (formValidation()) {
+      try {
+        const res = await axios.post("http://localhost:2000/auth/signin", {
+          email,
+          password,
+        });
+        console.log("login response:", res.data);
+      } catch (error) {
+        console.error("Login error:", error);
+        setLoginError("An error occurred while attempting to log in.");
+      }
     }
   };
+
+  useEffect(() => {
+    if (localStorage.getItem("userId")) {
+      window.location.href = "profile";
+    }
+  }, []);
+
   return (
     <div>
-      <label>
-        Email:
-        <input
-          onChange={(e) => setEmail(e.target.value)}
-          type="email"
-          name="email"
-        />
-      </label>
-      <br />
-
-      <label>
-        password:
-        <input
-          onChange={(e) => setpassword(e.target.value)}
-          type="password"
-          name="password"
-        />
-      </label>
-      <br />
-
-      <button onClick={handleSubmit}> signin</button>
+      <h2>Sign in to your account</h2>
+      <form>
+        <div>
+          <label>Your email</label>
+          <input
+            placeholder="Enter your email address"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          {errormsg.email && <p>{errormsg.email}</p>}
+        </div>
+        <div>
+          <label>Your password</label>
+          <input
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {errormsg.password && <p>{errormsg.password}</p>}
+        </div>
+        <button type="submit" onClick={handleLogin}>
+          Login to your account
+        </button>
+      </form>
     </div>
   );
-};
-
-export default registrations;
+}
--- pages/user/profile.js
+++ pages/user/profile.js
@@ -1,7 +1,49 @@
+import axios from "axios";
+
 import React from "react";
+import { useState, useEffect } from "react";
 
-const profile = () => {
-  return <div>profile</div>;
-};
+export default function UserProfile() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    let res = localStorage.getItem("userId");
+    axios
+      .get("http://localhost:2000/auth/profile", { headers: { id: res } })
+      .then((res) => {
+        setData(res.data);
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-export default profile;
+  return (
+    <>
+      <div>
+        <div>
+          <h1>User Profile</h1>
+
+          <div>
+            <label>Name</label>
+            <p>{data.name}</p>
+          </div>
+
+          <div>
+            <label>Phone Number</label>
+            <p>{data.phone}</p>
+          </div>
+
+          <div>
+            <label>Email</label>
+            <p>{data.email}</p>
+          </div>
+          <div>
+            <label>Company Name</label>
+            <p>{data.companyName}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
